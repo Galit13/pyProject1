fdb5692235159bb15c7c438c0512567cc2c1b664
Galit <76530191+Galit13@users.noreply.github.com>
2020-12-26 18:17:27 +0200

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,6 @@
+# תרגיל 1- ממוצע
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+num4 = int(input("Enter the fourth number: "))
+print("The average of the numbers is", (num1 + num2 + num3 + num4) / 4)
---
+++ calculator.py
@@ -0,0 +1,29 @@
+import math
+
+x = int(input("Enter 1,2,3,4,5,6 for an action:"))
+
+if x <= 5:
+    if x >= 1:
+        n1 = int(input("Enter the first number:"))
+        n2 = int(input("Enter the first number:"))
+        if x == 1:
+            print(n1 + n2)
+        elif x == 2:
+            print(n1 - n2)
+        elif x == 3:
+            print(n1 * n2)
+        elif x == 4:
+            if n2 != 0:
+                print(n1 / n2)
+            else:
+                print("error")
+        else:
+            print(n1 ** n2)
+    elif x == 6:
+        n = int(input("Enter the number:"))
+        if n >= 0:
+            print(math.sqrt(n))
+        else:
+            print("error")
+    else:
+        print("wrong action")
